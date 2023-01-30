99f1cc8f3c1eccda8e9859a052a1daee0ecc22f2
Mioki <22417711+okibcn@users.noreply.github.com>
2023-01-29 21:22:34 -0800

    Cosmetics

--- wslcompact.ps1
+++ wslcompact.ps1
@@ -1,5 +1,10 @@
-# if(!$args) { "usage: wslcompact <distro>"; exit 1 }
-# Version v0.2
+#  WSL compact, v2.2023.01.29
+# 
+#  (C) 2023 Oscar Lopez. 
+#  For more information visit: https://github.com/okibcn/wslcompact
+# 
+
+$sf = 1.05
 $info = $false
 $help = $false
 $target_distros = foreach ($arg in $args) {
@@ -22,9 +27,10 @@ if ($help) {
 
     Compacts the image file of the DISTROS. If no distro is provided it will compact all the images.
     You can get a list of your installed distros with the command: wsl -l
+    NOTE: WSL will be shutdown for compacting the images.
 
     Options:
-        -i   run in info mode, providing data without compacting.
+        -i   runs in info mode, providing data and estimation without compacting.
         -h   prints this help
 
     Examples: wslcompact; wslcompact -i; wslcompact Ubuntu Kali
@@ -32,7 +38,6 @@ if ($help) {
     "
     exit 0
 }
-$sf = 1.05
 $tmp_folder = "$Env:TEMP\wslcompact"
 $freedisk = (Get-PSDrive $env:TEMP[0]).free
 mkdir "$tmp_folder" -ErrorAction SilentlyContinue | Out-Null
@@ -46,33 +51,34 @@ Get-ChildItem HKCU:\Software\Microsoft\Windows\CurrentVersion\Lxss\`{* | ForEach
         $size1 = (Get-Item -Path "$wsl_path\ext4.vhdx").Length / 1MB
         $estimated = ((wsl -d "$wsl_distro" -e df /) | select-string "\d (\d+) \d").Matches[0].Groups[1].Value
         $estimated = [long]$estimated * 1024
-        Write-Host " Processing: $wsl_distro distro"
-        Write-Host " Image file: $wsl_path\ext4.vhdx"
-        Write-Host " Image size: $size1 MB"
-        Write-Host " Estimating target size..." -NoNewLine
-        Write-Host ([long]($estimated / 1MB * ((($sf - 1) / 2) + 1)))"±"([long]($estimated / 1MB * ($sf - 1) / 2))"MB"
+        Write-Output " Processing: $wsl_distro distro"
+        Write-Output " Image file: $wsl_path\ext4.vhdx"
+        Write-Output " Image size: $size1 MB"
+        Write-Output " Estimating target size..." -NoNewLine
+        Write-Output ([long]($estimated / 1MB * ((($sf - 1) / 2) + 1)))"±"([long]($estimated / 1MB * ($sf - 1) / 2))"MB"
         if (($estimated * $sf) -lt $freedisk) {
              # There is enough free space in the TEMP drive
             if (!($info))
             {   # we are not in simulation mode.
-                Write-Host " Compacting image..." -NoNewLine
+                Write-Output " Compacting image..." -NoNewLine
                 wsl --shutdown
                 cmd /c "wsl --export ""$wsl_distro"" - | wsl --import wslclean ""$tmp_folder"" -" 
                 wsl --shutdown
                 Move-Item "$tmp_folder/ext4.vhdx" "$wsl_path" -Force
                 wsl --unregister wslclean | Out-Null
                 $size2 = (Get-Item -Path "$wsl_path\ext4.vhdx").Length / 1MB
-                Write-Host " Compacted from $size1 MB to $size2 MB`n"
+                Write-Output " Compacted from $size1 MB to $size2 MB`n"
             }        
         }
         else {
             # There isn't enough free space in the TEMP drive
-            write-Host " WARNING: there isn't enough free space in temp drive"(Get-PSDrive $env:TEMP[0])"to process $wsl_distro."
-            write-Host "          There are only"([long]($freedisk / 1MB))"MB available."
-            write-Host ""
-            write-Host " Please, change the TEMP folder to a drive with at least"([long]($estimated * $sf / 1MB))"MB of free space."
-            write-Host " You cand do it by typing `$env:TEMP=`"Z:/your/new/temp/folder`" before using wslcompact.`n"
+            write-Output " WARNING: there isn't enough free space in temp drive"(Get-PSDrive $env:TEMP[0])"to process $wsl_distro."
+            write-Output "          There are only"([long]($freedisk / 1MB))"MB available."
+            write-Output ""
+            write-Output " Please, change the TEMP folder to a drive with at least"([long]($estimated * $sf / 1MB))"MB of free space."
+            write-Output " You cand do it by typing `$env:TEMP=`"Z:/your/new/temp/folder`" before using wslcompact.`n"
         }
     }
 }
 Remove-Item -Recurse -Force "$tmp_folder"
+write-Output ""
